14abe9ffdd6636d883e1895166bb731d3de69a92
Jonas Marciulevičius <69348657+dzonasm@users.noreply.github.com>
2021-05-25 08:48:29 -0700

    Mantass7 (#5)
    * fe massive update
    
    * profile pic
    
    * editinimas
    
    Co-authored-by: zoosodas <gyvunas@gmail.com>

--- JS/signUp.js
+++ JS/signUp.js
@@ -4,25 +4,16 @@ let url = 'http://localhost:3000/api/v1'
 document.getElementById('form').addEventListener('submit', async (event) => {
     event.preventDefault()
 
-
-    let email = document.getElementById('email').value
-
-    let password = document.getElementById('password1').value
-    let confirmationPassword = document.getElementById('password2').value
-    let name = document.getElementById('name').value
-    let phoneNumber = document.getElementById('phoneNumber').value
-
+    let email = document.querySelector('#email').value
+    let password = document.querySelector('#password').value
+    let name = document.querySelector('#name').value
+    let phone = document.querySelector('#phone').value
+    let confirmPassword = document.querySelector('#confirmPassword').value
 
     if (!email || !password) return alert('Fill in form')
-
-
-
-    if (confirmationPassword == ''){
-        alert("Please enter confirm password");
-    }else if(password != confirmationPassword) {
-        alert("\nPassword did not match: Please try again");
-
-}
+    if (password !== confirmPassword ) {
+        return alert ('wrong password')
+    }
 
     let body = {
         email,
@@ -31,7 +22,6 @@ document.getElementById('form').addEventListener('submit', async (event) => {
         phone
 
     }
-
 
     let response = await fetch(`${url}/user/signUp`, {
         method: 'POST',
