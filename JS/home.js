6cb4a4632860ea115b11c4ed0168c353ef46913b
Aiwwa <as.savinis@gmail.com>
2021-05-26 22:08:46 +0300

    Modal model added

--- JS/home.js
+++ JS/home.js
@@ -25,26 +25,130 @@ const carSearchForm = document.querySelector("#carSearchForm");
 
 // ---Modal
 
-const modal = document.querySelectorAll('.card').forEach((el, index) => {
-  el.addEventListener('click', () => {
-    console.log(el)
+// const modal = document.querySelectorAll('.card').forEach((el, index) => {
+//   el.addEventListener('click', () => {
 
-    createModal()
-  })
-})
-
-
-const createModal = () => {
-  console.log('creating modal...')
-
-  const div = document.createElement('div')
-  div.classList.add('modal')
+//     createModal()
+//   })
+// })
 
 
+// const createModal = () => {
+//   console.log('creating modal...')
+
+//   //--- main container for bacground
+//   const div = document.createElement('div')
+//   div.classList.add('modal')
+
+//   //--- container for modal
+//   const div2 = document.createElement('div')
+//   div2.classList.add('modal-container')
+//   div.appendChild(div2)
+
+//   //--- Close button
+//   const div3 = document.createElement('div')
+//   div.classList.add('t-right')
+//   const i = document.createElement('i')
+//   i.classList.add('far', 'fa-times-circle', 'fa-2x')
+//   div3.appendChild(i)
+
+//   //--- Title car modal
+//   const div4 = document.createElement('div')
+//   div4.classList.add('modal-title', 'ma-top', 't-center')
+//   // div4.innerText = `${}`
+//   div4.innerText = 'Supra'
+
+//   // --- Div5 Img
+//   const div5 = document.createElement('div')
+//   div5.classList.add('modal-img', 'ma-top')
+//   div5.appendChild(img)
+//   const img = document.createElement('img')
+//   img.src = ''
+
+//   // --- Div6 car name and price
+//   const div6 = document.createElement('div')
+//   div6.classList.add('modal-info-1')
+
+//   const div6innerDiv1 = document.createElement('div')
+//   div6innerDiv1.classList.add('info-1-border')
+//   const pCar = document.createElement('p')
+//   // pCar.innerText = `${}`
+//   pCar.innerText = 'Supra'
+//   div6innerDiv1.appendChild(pCar)
 
 
-  body.appendChild(div)
-}
+//   const div6innerDiv2 = document.createElement('div')
+//   div6innerDiv2.classList.add('info-1-border', 't-right')
+//   const pPrice = document.createElement('p')
+//   // p.innerText = `${}`
+//   pPrice.innerText = '40 000e'
+//   div6innerDiv2.appendChild(pPrice)
+
+
+//   div6.appendChild(div6innerDiv1)
+//   div6.appendChild(div6innerDiv2)
+
+//   // --- Div7 car info
+//   const div7 = document.createElement('div')
+//   div7.classList.add('modal-info-2')
+
+//   const div7innerDiv1 = document.createElement('div')
+//   div7innerDiv1.classList.add('info-2-wrap')
+//   const Div7Title = document.createElement('div')
+//   Div7Title.classList.add('titles')
+//   //-
+//   const Div7TitleDiv1 = document.createElement('div')
+//   Div7TitleDiv1.classList.add('ma-top')
+//   // Div7TitleDiv1.innerText = `${}`
+//   Div7TitleDiv1.innerText = 'Make'
+
+//   const Div7TitleDiv2 = document.createElement('div')
+//   Div7TitleDiv2.classList.add('ma-top')
+//   // Div7TitleDiv1.innerText = `${}`
+//   Div7TitleDiv2.innerText = 'Modal'
+
+//   const Div7TitleDiv3 = document.createElement('div')
+//   Div7TitleDiv3.classList.add('ma-top')
+//   // Div7TitleDiv1.innerText = `${}`
+//   Div7TitleDiv3.innerText = 'Year'
+
+//   const Div7TitleDiv4 = document.createElement('div')
+//   Div7TitleDiv4.classList.add('ma-top')
+//   // Div7TitleDiv1.innerText = `${}`
+//   Div7TitleDiv4.innerText = 'Year'
+
+
+
+
+//   Div7Title.appendChild(Div7TitleDiv1)
+//   Div7Title.appendChild(Div7TitleDiv2)
+//   Div7Title.appendChild(Div7TitleDiv3)
+//   Div7Title.appendChild(Div7TitleDiv4)
+
+//   div7innerDiv1.appendChild(Div7Title)
+//   div7innerDiv1.appendChild(Div7Info)
+
+//   const div7innerDiv2 = document.createElement('div')
+//   div7innerDiv2.classList.add('info-2-wrap')
+
+
+
+//   div7.appendChild(div7innerDiv1)
+//   div7.appendChild(div7innerDiv2)
+
+
+
+
+//   div2.appendChild(div3)
+//   div2.appendChild(div4)
+//   div2.appendChild(div5)
+//   div2.appendChild(div6)
+//   div2.appendChild(div7)
+
+
+
+//   body.appendChild(div)
+// }
 
 
 
---
+++ JS/test.js
@@ -0,0 +1,72 @@
+
+let testVal = 'Bmw'
+
+
+const modal = document.querySelectorAll('.img-container img').forEach((el, index) => {
+  el.addEventListener('click', () => {
+
+    createModal(el)
+    const closeBtn = document.querySelector('.t-right i')
+
+
+    closeBtn.addEventListener('click', () => {
+      closeBtn.parentElement.parentElement.parentElement.remove()
+    })
+
+    console.log(el.src)
+  })
+})
+
+const createModal = (el) => {
+  const div = document.createElement('div')
+  div.classList.add('modal-model')
+
+  div.innerHTML = `
+  <div class="modal">
+  <div class="modal-container">
+
+      <div class="t-right">
+          <i class="far fa-times-circle fa-2x"></i>
+      </div>
+
+      <div class="modal-title ma-top t-center">Supra</div>
+
+      <div class="modal-img ma-top ">
+          <img src=${el.src} alt="">
+      </div>
+
+      <div class="modal-info-1">
+          <div class="info-1-border">
+              <p>Toyota Supra</p>
+          </div>
+          <div class="info-1-border t-right">
+              <p>40 000 e</p>
+          </div>
+      </div>
+
+      <div class="modal-info-2">
+          <div class="info-2-wrap">
+              <div class="titles">
+                  <div class="ma-top">Make</div>
+                  <div class="ma-top">Modal</div>
+                  <div class="ma-top">Year</div>
+                  <div class="ma-top">Milage</div>
+              </div>
+              <div class="title-info ma-left">
+                  <div class="ma-top">-----</div>
+                  <div class="ma-top">${testVal}</div>
+                  <div class="ma-top">iki baznycios km</div>
+                  <div class="ma-top">1991 m</div>
+              </div>
+          </div>
+          <div class="info-2-wrap">next flex div</div>
+       </div>
+
+  </div>
+</div>
+  `
+  body.appendChild(div)
+}
+
+
+
