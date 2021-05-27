97d2064b58618af6960e47c58042af7c0ae16c92
Jonas <jonas.marciulevicius@halopowered.com>
2021-05-27 18:28:02 +0300

    add loading cars in profile page

--- JS/profile.js
+++ JS/profile.js
@@ -33,28 +33,44 @@ const getMyPosts = async () => {
 	showPosts(data);
 };
 
+//     <div>
+//       <div>
+//      <button onclick="deleteCarPost('${car._id}')">x</button>
+
+//        <button class="edit-event" onclick="editCar(this, '${car._id}')"> Edit</button>
+//        <h3>Post</h3>
+//         <h5>Car Brand: ${car.carBrand}</h5>
+//         <p>Car Model: ${car.carModel}</p>
+//         <p>Car Description: ${car.carDescription}</p>
+//         <p>Car Mileage: ${car.carMileage}</p>
+//         <p>Car Year: ${car.carYear}</p>
+//         <p>Car Price: ${car.carPrice}</p>
+//         <div style="background-image: url('${car.carImage}'); height: 200px; background-size: cover "></div>
+//         <p>Created at: ${car.createdAt}</p>
+
+//   </div>
+
+// </div>
 const showPosts = data => {
 	for (let car of data) {
 		let card = `
-        <div>
-          <div>
-         <button onclick="deleteCarPost('${car._id}')">x</button>
-         
-           <button class="edit-event" onclick="editCar(this, '${car._id}')"> Edit</button>
-           <h3>Post</h3>
-            <h5>Car Brand: ${car.carBrand}</h5>
-            <p>Car Model: ${car.carModel}</p>
-            <p>Car Description: ${car.carDescription}</p>
-            <p>Car Mileage: ${car.carMileage}</p>
-            <p>Car Year: ${car.carYear}</p>
-            <p>Car Price: ${car.carPrice}</p> 
-            <div style="background-image: url('${car.carImage}'); height: 200px; background-size: cover "></div>
-            <p>Created at: ${car.createdAt}</p>
-            
-      </div>
-
-    </div>`;
-		let cardContainer = document.querySelector(".myPostContainer");
+	
+	<div class="card d-flex flex-column">
+            <div class="img-container">
+                <img class="d-flex" src=${
+					car.carImage ? car.carImage : "../images/jonathan-petersson-d3iHpnCI_Yg-unsplash.jpg"
+				} alt="">
+            </div>
+            <div class="car-card-info">
+                <div class="sub-info">
+                    <h5>${car.carBrand} ${car.carModel}</h5>
+                    <p>${car.carMileage}</p>
+                </div>
+                <span>${car.carPrice}$</span>
+            </div>
+        </div>
+	`;
+		let cardContainer = document.querySelector(".car-card-grid");
 		cardContainer.innerHTML += card;
 	}
 };
